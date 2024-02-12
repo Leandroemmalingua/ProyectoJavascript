c525e26a53c4e74028fe9d60ef959a2be495eab7
leandroemmalingua <leanemmalingua@gmail.com>
2024-02-12 17:43:37 -0300

    version2.0

--- js/index.js
+++ js/index.js
@@ -25,3 +25,47 @@ do {
 }
 
 elegir();
+
+
+const header = document.getElementById('header')
+console.log(header)
+
+class Producto {
+        constructor (id,nombre,descripcion,imagen,precio,){
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.imagen = imagen;
+        this.precio = precio;
+        }
+	}
+
+
+const productosArray = [
+    new Producto ("1","Hamburguesa con JyQ","MedallondeCarne+Jamon+Queso","url","1500"),
+    new Producto ("2","Hamburguesa Completa","MedallondeCarne+Jamon+Queso+Lechuga+Tomate","url","1800"),
+    new Producto ("3","Hamburguesa Cochina","MedallondeCarne+Cheddar+HuevoFrito","url","1900"),
+    new Producto ("4","Hamburguesa Baccon","MedallondeCarne+Cheddar+Baccon","url","2100"),
+    new Producto ("5","Hamburguesa Crispy","MedallondePollo+Lechuga+Tomate+Jamon+Queso","url","1600")
+]
+
+const contenedorProductos = document.getElementById("grilla-productos");
+
+function listadoProductos(productos){
+    productos.forEach(producto => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        card.innerHTML = `
+                            <h2>"${producto.nombre}"</h2>
+                            <h3>"${producto.descripcion}"</h3>
+                            <img src="${producto.imagen}" alt="">
+                            <p>precio:$ ${producto.precio}</p>
+                            `
+        contenedorProductos.appendChild(card);
+    });
+}
+
+listadoProductos(productosArray);
+
+
+
