1eb7cbd38867c122dca3ad76efc2b473b519fe3b
leandroemmalingua <leanemmalingua@gmail.com>
2024-02-14 20:08:45 -0300

    version2.1

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-let nombre = prompt("Ingrese su Nombre");
+/*let nombre = prompt("Ingrese su Nombre");
 let apellido = prompt("Ingrese su Apellido");
 let nombreCompleto = nombre + " " + apellido
 let regristado = prompt("Usted esta Registrado?ingrese si/no");
@@ -24,7 +24,7 @@ do {
     }   while (cod!=4);
 }
 
-elegir();
+elegir(); */
 
 
 const header = document.getElementById('header')
@@ -40,8 +40,9 @@ class Producto {
         }
 	}
 
+let carrito = []
 
-const productosArray = [
+const productos = [
     new Producto ("1","Hamburguesa con JyQ","MedallondeCarne+Jamon+Queso","url","1500"),
     new Producto ("2","Hamburguesa Completa","MedallondeCarne+Jamon+Queso+Lechuga+Tomate","url","1800"),
     new Producto ("3","Hamburguesa Cochina","MedallondeCarne+Cheddar+HuevoFrito","url","1900"),
@@ -65,7 +66,62 @@ function listadoProductos(productos){
     });
 }
 
-listadoProductos(productosArray);
+listadoProductos(productos);
 
+let seleccion = prompt("Hola desea realizar una compra? si o no");
 
+while(seleccion != "si" && seleccion != "no"){
+    alert("Porfavor ingresa si o no")
+    seleccion = prompt("Desea realizar una compra? si o no")
+}
 
+if (seleccion == "si"){
+    alert("A continuacion podra elegir de nuestra lista")
+    let todosLosProductos = productos.map((producto) => producto.nombre + " " + producto.precio + "$")
+    alert(todosLosProductos.join(" - "))
+}else if (seleccion == "no"){
+    alert("gracias por elegirnos, hasta luego")
+}
+
+while (seleccion != "no"){
+    let producto = prompt("Agrega otro producto a tu carrito")
+    let precio = 0
+    if (producto == "Hamburguesa con JyQ" || producto == "Hamburguesa Completa" || producto == "Hamburguesa Cochina" || producto == "Hamburguesa Baccon" || producto == "Hamburguesa Crispy" ){
+        switch(producto) {
+            case "Hamburguesa con JyQ":
+            precio = 1500;
+            break;
+            case "Hamburguesa Completa":
+            precio = 1800;
+            break;
+            case "Hamburguesa Cochina":
+            precio = 1900;
+            break;
+            case "Hamburguesa Baccon":
+            precio = 2100;
+            break;
+            case "Hamburguesa Crispy":
+            precio = 1600;
+            break;
+        default:
+            break;
+        }
+    
+    let cantidad = parseInt(prompt("Cuantas unidades desea Comprar?"))
+    
+    carrito.push({producto, cantidad, precio})
+    console.log(carrito);
+    } else {
+        alert ("No disponible")
+    }
+
+    seleccion = prompt("Desea agregar Algo mas?")
+
+    while (seleccion === "no"){
+        alert("Gracias por su compra")
+        carrito.forEach((compraFinal) => {
+            console.log(`producto: ${compraFinal.producto}, unidades: ${compraFinal.cantidad}, total a pagar: ${compraFinal.cantidad * compraFinal.precio}`)
+        })
+    break;
+    }
+}
